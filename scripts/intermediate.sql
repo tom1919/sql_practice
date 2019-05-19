48f93cf979173093b29c29399f1236bc928f796c
tom1919 <tommy_lang@users.noreply.github.com>
2019-05-18 23:37:33 -0400

    added subquery examples

--- scripts/intermediate.sql
+++ scripts/intermediate.sql
@@ -1,7 +1,7 @@
 -- dealing with null values
 -- date functions
 -- math functions
--- case when
+-- subqueries
 
 -------------------------------- null values
 
@@ -68,21 +68,185 @@ select
 	round(TotalDue, 0, 1) as truncate_valueGO
 from
 	sales.SalesOrderHeader
-	
--------------------------------------- case when functions
 
+------------------------------------------- subqueries
+
+-- subquery in select clause
+-- number of sales people per territory
 select
-	SalesOrderID,
-	TotalDue,
-	case
-		when TotalDue < 100
-		and TerritoryID = 1 then 'less than 100 in T1'
-		when TotalDue BETWEEN 100 and 500
-		and TerritoryID = 1 then 'between 100 and 500 in T1'
-		else 'greater than 500 or not in T1'
-	end as total_due_description
+	distinct st.name,
+	(
+		select count(*)
+	from
+		Sales.SalesPerson sp
+	where
+		st.territoryid = sp.TerritoryID) as num
 from
-	sales.SalesOrderHeader
+	sales.SalesTerritory st;
+
+-- subquery inside the from clause
+-- number of sales person in each territory
+select
+	st.name,
+	subquery.cnt
+from
+	sales.SalesTerritory st
+inner join (
+	select
+		count(*) as cnt,
+		sp.Territoryid as Territoryid
+	from
+		sales.salesperson sp
+	group by
+		TerritoryID) subquery on
+	st.TerritoryID = subquery.Territoryid;
+
+
+-- subquery inside the from clause. 2nd example
+-- the orders of each product that had the max order qty	
+select
+	sod.SalesOrderID,
+	sod.ProductID,
+	sod.OrderQty
+from
+	sales.SalesOrderDetail sod
+inner join (
+	select
+		max(OrderQty) max_qty,
+		ProductID
+	from
+		sales.SalesOrderDetail
+	group by
+		ProductID) sq on
+	sod.OrderQty = sq.max_qty
+	and sod.ProductID = sq.productid;
+
+
+-- subquery in where clause
+-- bonuses that are greater than avg
+select
+	*
+from
+	Sales.SalesPerson
+where
+	Bonus > (
+	select
+		AVG(bonus)
+	from
+		sales.SalesPerson);
+	
+
+-- correlated subqueries. reference column(s) in main query
+-- evaluated once per row so its slow
+-- sales persons that have bonus of 5000
+SELECT
+	DISTINCT 
+	p.LastName,
+	p.FirstName,
+	e.BusinessEntityID
+FROM
+	Person.Person AS p
+JOIN HumanResources.Employee AS e ON
+	e.BusinessEntityID = p.BusinessEntityID
+WHERE
+	5000.00 IN (
+		SELECT Bonus
+	FROM
+		Sales.SalesPerson sp
+	WHERE
+		e.BusinessEntityID = sp.BusinessEntityID);
+	
+
+-- nested subqueries
+-- useful for multi step transformations
+-- each sales person total sales for latest year and their sales quota
+select
+	sq2.salespersonid,
+	sq2.total_sales,
+	sp.SalesQuota
+from
+	(
+	select
+		soh.SalesPersonID,
+		sum(soh.SubTotal) as total_sales
+	from
+		sales.SalesOrderHeader soh
+	where
+		year(OrderDate) = (
+		select
+			max(year(orderdate))
+		from
+			sales.SalesOrderHeader)
+	group by
+		soh.SalesPersonID ) as sq2
+left join Sales.SalesPerson sp on
+	sq2.salespersonid = sp.BusinessEntityID
+		
+-- common table expressions (CTE)
+-- each sales person total sales for latest year and their sales quota
+-- organize prev subqueries sequentially 
+-- created cte must be called, error otherwise
+
+-- innermost subquery
+with latest_year  as (
+select
+	max(year(orderdate)) as yyyy
+from
+	sales.SalesOrderHeader 
+	),
+-- outer subquery
+sales_cte as (
+select
+	soh.SalesPersonID,
+	sum(soh.SubTotal) as total_sales
+from
+	sales.SalesOrderHeader soh
+where
+	year(OrderDate) = (
+		select yyyy
+	from
+		latest_year
+		)
+group by
+	soh.SalesPersonID 
+	)
+
+-- join sales cte
+select
+	sales_cte.salespersonid,
+	sales_cte.total_sales,
+	sp.SalesQuota
+from
+	sales_cte
+left join Sales.SalesPerson sp on
+	sales_cte.salespersonid = sp.BusinessEntityID
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
